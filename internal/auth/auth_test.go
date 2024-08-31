bf8508092bceeda75d3270b380233ec067c97c81
amengdv <aminkafri0082@gmail.com>
2024-08-31 19:03:23 +0800

    Added formatting check

--- internal/auth/auth_test.go
+++ internal/auth/auth_test.go
@@ -6,30 +6,30 @@ import (
 )
 
 func TestGetApiKey(t *testing.T) {
-    tests := []struct{
-        name string
-        input http.Header
-        expected string
-    }{
-        {
-            name: "Test against random",
-            input: http.Header{
-                "Authorization": []string{"ApiKey test123"},
-            },
-            expected: "test123",
-        },
-    }
+	tests := []struct {
+		name     string
+		input    http.Header
+		expected string
+	}{
+		{
+			name: "Test against random",
+			input: http.Header{
+				"Authorization": []string{"ApiKey test123"},
+			},
+			expected: "test123",
+		},
+	}
 
-    for _, tc := range tests {
-        t.Run(tc.name, func(t *testing.T) {
-            actual, err := GetAPIKey(tc.input)
-            if err != nil {
-                t.Errorf("Error: %v\n", err)
-            }
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			actual, err := GetAPIKey(tc.input)
+			if err != nil {
+				t.Errorf("Error: %v\n", err)
+			}
 
-            if actual != tc.expected {
-                t.Errorf("FAIL! Expect %v Got %v\n", tc.expected, actual)
-            }
-        })
-    }
+			if actual != tc.expected {
+				t.Errorf("FAIL! Expect %v Got %v\n", tc.expected, actual)
+			}
+		})
+	}
 }
